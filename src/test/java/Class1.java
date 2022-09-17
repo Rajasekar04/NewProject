fc573e710345f15c3be0da8189b5a4b092ba2d11
Rajasekar04 <iamragi04@gmail.com>
2022-09-17 18:31:52 +0530

    message

---
+++ src/test/java/Class1.java
@@ -0,0 +1,16 @@
+
+public class Class1 {
+	
+	private void empId() {
+		System.out.println();
+	}
+	private void empName() {
+		System.out.println();	
+
+	}
+	private void empJob() {
+		System.out.println();
+
+	}
+
+}
